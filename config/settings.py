119debc01fb6321cf14d729c02d651d67ea31765
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2022-11-03 16:56:42 +0500

    fixed

--- config/settings.py
+++ config/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = 'home/zohidillo/turizm/static/'
-# STATIC_ROOT = 'home/zohidillo/turizm/static/'
+# STATIC_URL = '/home/zohidillo/turizm/static/'
+# STATIC_ROOT = '/home/zohidillo/turizm/static/'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile/')
@@ -140,8 +140,8 @@ STATICFILES_DIRS = [
 ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
-# MEDIA_URL = 'home/zohidillo/turizm/media/'
-# MEDIA_ROOT = 'home/zohidillo/turizm/media/'
+# MEDIA_URL = '/home/zohidillo/turizm/media/'
+# MEDIA_ROOT = '/home/zohidillo/turizm/media/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
