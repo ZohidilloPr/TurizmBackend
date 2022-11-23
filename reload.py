59074ded1fa3a8f1e8fe3e868a763f38d1f3e768
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2022-11-23 16:42:15 +0500

    add files

--- reload.py
+++ reload.py
@@ -1,6 +1,6 @@
 import os
 
-os.system("sudo systemctl restart daemon-reload")
+os.system("sudo systemctl daemon-reload")
 os.system("sudo systemctl restart turizm")
 os.system("sudo systemctl restart nginx")
 os.system("sudo nginx -t")
